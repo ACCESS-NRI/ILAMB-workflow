031e7afe4b8204c0413694f8bbf112b86c49e12e
rhaegar325 <65888230+rhaegar325@users.noreply.github.com>
2023-06-08 13:05:07 +1000

    Update softlink_builder.py

--- slink_to_access-esm1-5/softlink_builder.py
+++ slink_to_access-esm1-5/softlink_builder.py
@@ -9,10 +9,7 @@ args = parser.parse_args()
 
 
 Path='/g/data/fs38/publications/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historical/'
-# Path_local=os.path.dirname(os.path.abspath(__file__))
-# Experiment='r38i1p1f1'
 
-# f=os.walk(Path+Experiment)
 
 dic={
     'Emon':['cSoil'],
@@ -40,32 +37,3 @@ for ex in exlist:
                         dist=local_path+'/'+Experiment+'/'+subfolder+'.nc'
                         os.symlink(src,dist)
                         break
-
-                # src=Path+Experiment+'/'+folder+'/'+subfolder+'/gn/files'
-                # dist=os.path.dirname(os.path.abspath(__file__))+'/'+Experiment+'/'+subfolder
-                # os.symlink(src,dist)
-        
-
-
-# for dirpath,dirnames,filenames in f:
-
-#     for folder in dirnames:
-#         # list_d=['Amon','Emon','LImon','Lmon','Omon','SImon']
-#         if(folder in dic.keys()):
-#             subf=os.walk(dirpath+'/'+folder)
-#             for subpath,subnames,subfilenames in subf:
-#                 for subfolder in subnames:
-#                     src=subpath+'/'+subfolder+'/gn/files'
-#                     dist=os.path.dirname(os.path.abspath(__file__))+'/'+subfolder
-
-#                     os.symlink(src,dist)
-#                 break
-
-#     break
-
-
-# for folder in dic.keys():
-#     for subfolder in dic[folder]:
-#         src=Path+Experiment+'/'+folder+'/'+subfolder+'/gn/files'
-#         dist=os.path.dirname(os.path.abspath(__file__))+'/'+subfolder
-#         os.symlink(src,dist)
