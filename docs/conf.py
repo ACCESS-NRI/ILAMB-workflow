366b1ea072a4e2c7b40d19b352cb7d5094907191
rbeucher <romain.beucher@anu.edu.au>
2023-08-21 20:34:05 +1000

    Add sphinx_markdown_tables to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
     "recommonmark",
+    "sphinx_markdown_tables",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
